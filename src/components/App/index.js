b368289d28f6edc39a9bc92fc400c2d64d71432e
Carlos E Alford M <carlos.alford@gmail.com>
2022-06-14 23:48:50 +0100

    Refactor useState to useReducer moving logic of CommentForm and CommentList components down a level

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,23 +1,29 @@
-import {blog} from '../../data/blogs.js';
-import {useState} from 'react';
+import blog from '../../data/blogs.js';
+import comments from '../../data/comments.js'
+import {useState, useReducer} from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import BlogPost from '../BlogPost'
-import CommentList from '../CommentList';
-import CommentForm from '../CommentForm';
 
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'ADD_COMMENT':
+      return [...state, {id: uuidv4(), ...action.comment}];
+    default:
+      return state;
+  }
+}
 
 function App() {
-  const [comments, setComments] = useState([{id: uuidv4(), author: 'Carlos Alford', content: 'The very first comment'}]);
+  const [allComments, dispatch] = useReducer(reducer, comments);
 
-  function handleCommentSubmit({author, comment}) {
-    setComments([...comments, {id: uuidv4(), author, content: comment}])
+  function handleCommentSubmit(comment) {
+    dispatch({type: 'ADD_COMMENT', comment})
   }
 
   return (
     <main>
-      <BlogPost blog={blog} />
-      <CommentList comments={comments} />
-      <CommentForm onSubmit={handleCommentSubmit} />
+      {blog.map((post, index) => <BlogPost post={post} key={index} comments={allComments} handleSubmit={handleCommentSubmit} />)}
     </main>
   );
 }
