a2571f7cd3d284a79f87c74cefabf84c60100e2a
Carlos E Alford M <carlos.alford@gmail.com>
2022-06-14 21:15:11 +0100

    Add comments dummy data

---
+++ src/data/comments.js
@@ -0,0 +1,23 @@
+const comments=[
+    {
+      postID: "kskBC5webqgNQUishuriken6q",
+      id: "kskBC5HZ8qgNQUiW6If6q",
+      author: "Billy Bootcamper",
+      content: "Hello, great post",
+    },
+    {
+      postID: "kskBC5webqgNQUishuriken6q",
+      id: "jFyGAKz1VsGputO1gV8xa",
+      author: "Chris Meah",
+      content: "Many thank yous",
+    },
+    {
+      postID: "kskweb5HZ8qgshurikenNQUiW",
+      id: "jFyGAdfffVsGputh5tO1ga",
+      author: "Chris Meah",
+      content: "Many thank yous",
+    },
+    
+  ];
+  
+export default comments;
