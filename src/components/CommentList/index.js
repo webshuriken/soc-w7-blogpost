c72896fe0aabd0d7b03fdb1df7c20c49e92e00d8
Carlos E Alford M <carlos.alford@gmail.com>
2022-06-14 23:08:50 +0100

    Update CommentList component to take in another prop and reduce only the messages for the blog post being rendered

--- src/components/CommentList/index.js
+++ src/components/CommentList/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import Comment from '../Comment';
 
 
-function CommentList({comments}) {
+function CommentList({postID, comments}) {
+  // create a list for the comments matching the current blog post id
+  const postComments = comments.reduce((acc, comment) => {
+    return postID === comment.postID ? [...acc, <Comment author={comment.author} content={comment.content} key={comment.id} />] : [...acc];
+  }, []);
   return (
     <section className="comments-wrapper">
-      {comments.map(comment => <Comment author={comment.author} content={comment.content} key={comment.id} />)}
+      {postComments}
     </section>
   );
 }
