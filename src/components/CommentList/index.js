6188158f6cee0caf51096a2fabcfa4761b492df5
Carlos E Alford M <carlos.alford@gmail.com>
2022-06-16 08:03:08 +0100

    Refactor CommentList component to use reduce inside render

--- src/components/CommentList/index.js
+++ src/components/CommentList/index.js
@@ -3,13 +3,11 @@ import Comment from '../Comment';
 
 
 function CommentList({postID, comments}) {
-  // create a list for the comments matching the current blog post id
-  const postComments = comments.reduce((acc, comment) => {
-    return postID === comment.postID ? [...acc, <Comment author={comment.author} content={comment.content} key={comment.id} />] : [...acc];
-  }, []);
   return (
     <section className="comments-wrapper">
-      {postComments}
+      {comments.reduce((acc, comment) => {
+        return postID === comment.postID ? [...acc, <article><Comment author={comment.author} content={comment.content} key={comment.id} /></article>] : [...acc];
+      }, [])}
     </section>
   );
 }
