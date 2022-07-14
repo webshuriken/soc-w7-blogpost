32755f50d479466198c78c28950f89c3f639117f
Carlos E Alford M <carlos.alford@gmail.com>
2022-07-14 07:45:41 +0100

    Connect app to backend

--- src/reducers/commentsReducer.js
+++ src/reducers/commentsReducer.js
@@ -3,6 +3,8 @@ import { v4 as uuidv4 } from 'uuid';
 
 export function commentsReducer(state, action) {
 	switch(action.type) {
+		case 'POPULATE_COMMENTS':
+			return [...action.value];
 		case 'ADD_COMMENT':
 			return [...state, {id: uuidv4(), ...action.comment}];
 		default:
